1fb0518d4c31de51ce81d3fd8c25a75c2786bf16
Deepa N C <deepanc2001@gmail.com>
2023-08-29 11:30:03 +0530

    data service

--- hotelBooking/src/app/data.service.ts
+++ hotelBooking/src/app/data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import jsonData from '../assets/hotel_data.json';
 
 @Injectable({
@@ -7,9 +6,6 @@ import jsonData from '../assets/hotel_data.json';
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
-
-  // Method to fetch JSON data
   getJsonData(): any {
     return JSON.parse(JSON.stringify(jsonData));
   }
