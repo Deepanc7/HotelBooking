a3c195dbfb034899ed7ca30c59a297cefe9157f3
Deepa N C <deepanc2001@gmail.com>
2023-09-27 12:53:23 +0530

    changes in server side folder

--- HotelBookingFinal/src/main/java/com/example/HotelBookingFinal/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.HotelBookingFinal.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.HotelBookingFinal.model.Booking;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-}
