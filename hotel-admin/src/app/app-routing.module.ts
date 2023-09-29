5568f6f39936c767148f4583cfb250dff5634e9e
Palguni H <23149@omnicell.com>
2023-09-29 19:07:24 +0530

    Merged logic for uploading images and storing in cloudinary

--- hotel-admin/src/app/app-routing.module.ts
+++ hotel-admin/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ViewHotelComponent } from './view-hotel/view-hotel.component';
 import { EditHotelComponent } from './edit-hotel/edit-hotel.component';
 
 const routes: Routes = [
-  {path : '',component : SignInComponent},{component: ListHotelComponent, path: 'allHotels' },
+  {path : '',component : SignInComponent},
+  {component: ListHotelComponent, path: 'allHotels' },
   { component: AddHotelComponent, path: 'addHotel' },
   { component: ViewHotelComponent, path: 'viewHotel/:index' },
   { component: EditHotelComponent, path: 'editHotel/:index'}
